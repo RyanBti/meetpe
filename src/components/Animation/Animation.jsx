9fe2308f5b18ce577fb441f8a2d046182b4ef264
Ryan <ryan.annic@bti-advisory.com>
2024-12-06 15:50:12 +0100

    Modification des photos

--- src/components/Animation/Animation.jsx
+++ src/components/Animation/Animation.jsx
@@ -1,6 +1,12 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './Animation.css';
 
+import meetpeLogo from '../../../public/images/AnimationPicture/AppMeetPeLogo.webp';
+import meetpeUser from '../../../public/images/AnimationPicture/AppMeetPeUser.webp';
+import meetpeExp from '../../../public/images/AnimationPicture/AppMeetPeExp.webp';
+import meetPeMatch from '../../../public/images/AnimationPicture/AppMeetPeMatch.webp';
+import meetPeCreate from '../../../public/images/AnimationPicture/AppMeetPeCreate.webp';
+
 function Animation() {
     const animationRef = useRef(null); // Référence pour le conteneur
     const [isAnimated, setIsAnimated] = useState(false); // État de déclenchement de l'animation
@@ -36,11 +42,11 @@ function Animation() {
                 <div className="mask-left"></div>
                 <div className="mask-right"></div>
                 <div className={`background ${isAnimated ? 'animate' : ''}`}>
-                    <img src='../../../public/images/AnimationPicture/AppMeetPeExp.webp' alt="Experience" className="side-photo left-photo" />
-                    <img src='../../../public/images/AnimationPicture/AppMeetPeUser.webp' alt="User" className="side-photo left-photo" />
-                    <img src='../../../public/images/AnimationPicture/AppMeetPeLogo.webp' alt="Logo" className="center-photo" />
-                    <img src='../../../public/images/AnimationPicture/AppMeetPeMatch.webp' alt="Match" className="side-photo right-photo" />
-                    <img src='../../../public/images/AnimationPicture/AppMeetPeCreate.webp' alt="Create" className="side-photo right-photo" />
+                    <img src={meetpeExp} alt="Experience" className="side-photo left-photo" />
+                    <img src={meetpeUser} alt="User" className="side-photo left-photo" />
+                    <img src={meetpeLogo} alt="Logo" className="center-photo" />
+                    <img src={meetPeMatch} alt="Match" className="side-photo right-photo" />
+                    <img src={meetPeCreate} alt="Create" className="side-photo right-photo" />
                 </div>
             </div>
         </div>

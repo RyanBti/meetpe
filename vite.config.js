8d2028f27698336a9fcd21668d14c89a557f1496
Ryan <ryan.annic@bti-advisory.com>
2024-12-06 13:18:08 +0100

    Correction du chemin d'image et configuration de Vite

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', 
-  server: {
-    mimeTypes: {
-      "application/javascript": ["js"], 
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        // Ignore les erreurs spécifiques liées à la résolution
+        if (warning.code === 'UNRESOLVED_IMPORT') return;
+        warn(warning); // Log les autres avertissements
+      },
     },
   },
 });
